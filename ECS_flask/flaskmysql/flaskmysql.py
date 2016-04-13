6c76904e1d383e9782f562fcc36ab43e145af868
cym1021 <715812008@qq.com>
2016-04-13 16:43:54 +0800

    Update flaskmysql.py
    添加加密
    
    --eason

--- ECS_flask/flaskmysql/flaskmysql.py
+++ ECS_flask/flaskmysql/flaskmysql.py
@@ -4,29 +4,34 @@ import model
 import config as config
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = 'Q\xc5h\x93\xf4s\x87\xe1\xdf\x0c6r\xb8q\x07\xd6-\xc5\xb8\x1ew1\x8c\xfa'
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/user/register/<user_username>&<user_userpasswd>',methods=['GET','POST'])
+def register(user_username,user_userpasswd):
+    result=config.session.query(model.user).\
+       from_statement(text("SELECT * from user where userName=:username")).\
+        params(username=user_username).first()
+    if (result is None):
+        new_user = model.user(userName=user_username,userPasswd=user_userpasswd)
+        config.session.merge(new_user)
+        config.session.commit()
+        config.db.session.close()
+        return 'succeed'
+    return 'username is already exist!'
 
-# Return user info
-@app.route('/user/<user_id>', methods=['GET', 'POST'])
-def user(user_id):
-    result = model.user.query.filter_by(userId=user_id).first()
-    if result is None:
-        json_result = {'user_id': None}
-        return json.dumps(json_result, ensure_ascii=False)
+@app.route('/user/login/<user_username>&<user_userpasswd>',methods=['GET','POST'])
+def login(user_username,user_userpassword):
+    result=config.session.query(model.user).\
+        from_statement(text("select * from user where userName=:username")).\
+        params(username=user_username).first()
+    if result.check_password_hash(user_userpassword):
+        return 'succeed login'
     else:
-        json_result = {'user_id': result.userId,
-                       'user_Name': result.userName,
-                       'user_Email': result.userEmail,
-                       'user_Mobile': result.userMobile,
-                       'user_Passwd': result.userPasswd,
-                       'user_Ispaid': result.userIspaid}
-        return json.dumps(json_result, ensure_ascii=False)
-
+        return 'error username or password'
 
 #Return the state of the sensor
 @app.route('/state/<device_id>&<user_id>', methods=['GET'])
@@ -45,7 +50,6 @@ def state(user_id, device_id):
                        'pm': result.pm,
                        'smoke': result.smoke,
                        'updatetime':result.update_time}
-        config.session.close()
         return json.dumps(json_result, ensure_ascii=False)
 
 
