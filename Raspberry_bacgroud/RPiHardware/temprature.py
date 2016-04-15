beb65aa1afbec7f7462b78513a1c3f0ab4d97643
cym1021 <715812008@qq.com>
2016-04-15 20:28:18 +0800

    Update temprature.py

--- Raspberry_bacgroud/RPiHardware/temprature.py
+++ Raspberry_bacgroud/RPiHardware/temprature.py
@@ -2,44 +2,49 @@
 # -*- coding: UTF-8 -*-
 __author__ = 'zhkmx'
 from time import sleep
-
+import RPi.GPIO as GPIO
+import time
 ##在此写入有关于温度获取的函数
-def get_humidity_temprature(th):
+def get_humidity_temprature():
     channel = 18
     data = []
     j = 0
-    
+
+    #print "safe"
+    GPIO.setmode(GPIO.BCM)
     GPIO.setup(channel, GPIO.OUT)
     GPIO.output(channel, GPIO.LOW)
-    time.sleep(0.02)
+
+    sleep(0.02)
     GPIO.output(channel, GPIO.HIGH)
     GPIO.setup(channel, GPIO.IN)
-    
+    #print 'after sleep'
     while GPIO.input(channel) == GPIO.LOW:
         continue
     while GPIO.input(channel) == GPIO.HIGH:
-	continue
+        continue
     while j < 40:
-	k = 0
-	while GPIO.input(channel) == GPIO.LOW:
-	    continue
-	while GPIO.input(channel) == GPIO.HIGH:
+        k = 0
+        while GPIO.input(channel) == GPIO.LOW:
+            continue
+        while GPIO.input(channel) == GPIO.HIGH:
             k += 1
-	    if k > 100:
-		break
-	if k < 8:
-	    data.append(0)
-	else:
-	    data.append(1)
+            if k > 100:
+                break
+        if k < 8:
+            data.append(0)
+        else:
+            data.append(1)
 
-	j += 1
-
+        j += 1
+    GPIO.cleanup()
+    #print 'Get'
     humidity_bit = data[0:8]
     humidity_point_bit = data[8:16]
     temperature_bit = data[16:24]
     temperature_point_bit = data[24:32]
     check_bit = data[32:40]
-
+    #print 'Analyzed'
     humidity = 0
     humidity_point = 0
     temperature = 0
@@ -47,19 +52,23 @@ def get_humidity_temprature(th):
     check = 0
 
     for i in range(8):
-	humidity += humidity_bit[i] * 2 ** (7-i)
-	humidity_point += humidity_point_bit[i] * 2 ** (7-i)
-	temperature += temperature_bit[i] * 2 ** (7-i)
-	temperature_point += temperature_point_bit[i] * 2 ** (7-i)
-	check += check_bit[i] * 2 ** (7-i)
+        humidity += humidity_bit[i] * 2 ** (7-i)
+        humidity_point += humidity_point_bit[i] * 2 ** (7-i)
+        temperature += temperature_bit[i] * 2 ** (7-i)
+        temperature_point += temperature_point_bit[i] * 2 ** (7-i)
+        check += check_bit[i] * 2 ** (7-i)
 
     tmp = humidity + humidity_point + temperature + temperature_point
-
+    dd = {
+        'temp': 0,
+        'humid': 0
+        }
+    print 'tmp',tmp
     if check == tmp:
-        if th == 't':
-            return temperature
-        else:
-            return humidity
+        #print 'tttttttyyyyyyy'
+        dd['temp'] = temperature
+        dd['humid'] = humidity
+        return dd
     else:
         return -1000
 
@@ -70,29 +79,24 @@ def get_humidity_temprature(th):
 def commit_temprature(dict):
     i = 0
     while True:
-        temprature = get_humidity_temprature('t')
-        humidity = get_humidity_temprature('h')
-        if (temprature == -1000 or humidity == -1000):
+        dd = get_humidity_temprature()
+        #temprature = dd['temp']
+        #humidity = dd['humid']
+        if (dd == -1000):
             i = i + 1
         else:
-            dict['temp'] = temprature    #传入Manager临界资源值，该表达式右值应为温度获取有关函数所返回的实时温度*（此处的1为测试值）
-            dict['humid'] = humidity
+            dict['temp']=dd['temp']
+            dict['humid']=dd['humid']
+            #dict['temp'] = temprature    #传入Manager临界资源值，该表达式右值应为温度获取有关函数所返回的实时温度*（此处的1为测试值）
+            #dict['humid'] = humidity
             i = 0
+
         if i == 50:
             i = 0
             dict['temp'] = -1000
             dict['humid'] = -1000
-        print 'updated1'
+
+        print dict['temp']
+        print dict['humid']
         sleep(1)            #同步标注时间1s
     return
-
-
-
- # dict = {
- #            'temp': 0,
- #            'humid': 0,
- #            'fire':0,
- #            'smoke':0,
- #            'gas':0,
- #            'pm':0
- #            }
